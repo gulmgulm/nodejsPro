8f29989aec42676ab6f8d43d7dbe504f3f1321f0
gulmgulm <konlox@163.com>
2017-06-16 10:10:58 +0800

    views/index.html

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@
  * Created by ljg on 2017/6/3.
  */
 module.exports={
-
+    mongdb:'mongodb://localhost/daik'
 }
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,10 @@
 /**
  * Created by ljg on 2017/6/3.
  */
+var mongoose=require('mongoose');
+var config=require('./config');
+
+moudule.exports=function(){
+    var db=mongoose.connect(config.mongdb);
+    require('')
+}
